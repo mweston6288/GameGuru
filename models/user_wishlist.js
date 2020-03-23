b68ba010a31d2f06434c6705aa9792f0fe3e8b6f
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-23 18:51:08 -0400

    Fixed style errors

--- models/user_wishlist.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes){
-  // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
-  const UserWishList = sequelize.define("User_Wishlist",{},{
-    timestamps: false
-  });
-
-  return UserWishList;
-};
