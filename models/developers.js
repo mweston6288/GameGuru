8c83d930d8608c0fca42e29289ed51d5a4ca8d82
Marc Maldonado <marc@telecomelite.com>
2020-03-23 15:05:32 -0400

    trying to fix the errors. I guess my tab is 4 spaces

--- models/developers.js
+++ models/developers.js
@@ -1,11 +1,10 @@
 module.exports = function(sequelize, DataTypes){
-    const Developer = sequelize.define("Developer",{
-      DeveloperID: DataTypes.INTEGER,
-      title: DataTypes.STRING
-    });
-  
+  const Developer = sequelize.define("Developer",{
+    DeveloperID: DataTypes.INTEGER,
+    title: DataTypes.STRING
+  });
     Developer.associate = function(models){
-      Developer.hasMany(models.Games);
-    };
-    return Developer;
+    Developer.hasMany(models.Games);
   };
+    return Developer;
+};
