326a647e99cec18a51e5408ddd53637c365220fa
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-23 18:38:33 -0400

    Fixed minor format errors

--- models/user_wishlist.js
+++ models/user_wishlist.js
@@ -1,6 +1,8 @@
 module.exports = function(sequelize, DataTypes){
   // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
-  const UserWishList = sequelize.define("User_Wishlist",{},{
+  const UserWishList = sequelize.define("User_Wishlist",{
+    Priority: DataTypes.INTEGER
+  },{
     timestamps: false
   });
 
