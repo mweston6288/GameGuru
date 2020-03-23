32b5cf29484e4561e6f170c521e5a60c1148276c
Marc Maldonado <marc@telecomelite.com>
2020-03-23 15:10:46 -0400

    Ok this should be it

--- models/developers.js
+++ models/developers.js
@@ -4,7 +4,7 @@ module.exports = function(sequelize, DataTypes){
     title: DataTypes.STRING
   });
   Developer.associate = function(models){
-  Developer.hasMany(models.Games);
+    Developer.hasMany(models.Games);
   };
-    return Developer;
+  return Developer;
 };
