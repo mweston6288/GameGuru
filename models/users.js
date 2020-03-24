96d5c5d7b9d719592de36ae7886d129871d27646
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-24 15:49:56 -0400

    Signup page now saves accounts on run

--- models/users.js
+++ models/users.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("Users",{
     username: {
@@ -17,5 +19,16 @@ module.exports = function(sequelize, DataTypes) {
     User.belongsToMany(models.Games, {through: "User_Library"});
     User.belongsToMany(models.Developer, {through: "User_Watchlist"});
   };
+
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  User.prototype.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a User is created, we will automatically hash their password
+  User.addHook("beforeCreate", function (user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+
   return User;
 };
