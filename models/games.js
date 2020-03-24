449e87b8773f65c0add018fb6a50be39fc82b6e3
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-24 18:24:24 -0400

    Changed spacing rules and updated fromat

--- models/games.js
+++ models/games.js
@@ -1,29 +1,29 @@
 module.exports = function(sequelize, DataTypes){
-  // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
-  const Games = sequelize.define("Games",{
-    gameID: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.STRING,
-    year: DataTypes.DATE,
-    score: DataTypes.INTEGER,
-    image:DataTypes.STRING,
-    ESRB:DataTypes.STRING,
-  },{
-    timestamps: false
-  });
-  Games.associate = function(models){
-    console.log(models);
-    //Games.belongsToMany(models.Platforms, {through: "Games_Platform"});
-    Games.belongsToMany(models.Users, {through: "User_Wishlist"});
-    Games.belongsToMany(models.Users, {through: "User_Library"});
-    Games.belongsTo(models.Developer,{
-      foreignKey: {
-        allowNull: false
-      }});
-    Games.belongsTo(models.Publisher,{
-      foreignKey: {
-        allowNull: false
-      }});
-  };
-  return Games;
+    // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
+    const Games = sequelize.define("Games",{
+        gameID: DataTypes.INTEGER,
+        title: DataTypes.STRING,
+        description: DataTypes.STRING,
+        year: DataTypes.DATE,
+        score: DataTypes.INTEGER,
+        image:DataTypes.STRING,
+        ESRB:DataTypes.STRING,
+    },{
+        timestamps: false
+    });
+    Games.associate = function(models){
+        console.log(models);
+        //Games.belongsToMany(models.Platforms, {through: "Games_Platform"});
+        Games.belongsToMany(models.Users, {through: "User_Wishlist"});
+        Games.belongsToMany(models.Users, {through: "User_Library"});
+        Games.belongsTo(models.Developer,{
+            foreignKey: {
+                allowNull: false
+            }});
+        Games.belongsTo(models.Publisher,{
+            foreignKey: {
+                allowNull: false
+            }});
+    };
+    return Games;
 };
--- routes/wishlist-api-routes.js
+++ routes/wishlist-api-routes.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
 module.exports = (app) => {
-  app.get("/api/user/wishlist", (req, res) => {
-    db.Wishlist.findAll({
-      where: devQuery, pubQuery,
-      include: [db.Developer] [db.Publisher]
-    }).then((dbWishlist) => {
-      res.json(dbWishlist);
+    app.get("/api/user/wishlist", (req, res) => {
+        db.Wishlist.findAll({
+            where: devQuery, pubQuery,
+            include: [db.Developer] [db.Publisher]
+        }).then((dbWishlist) => {
+            res.json(dbWishlist);
+        });
     });
-  });
 
-  app.get("/api/user/wishlist", (req, res) => {
-    db.Wishlist.findOne({
-      where:  {
-        id: req.params.id
-      },
-    }).then((dbWishlist) => {
-      res.json(dbWishlist);
+    app.get("/api/user/wishlist", (req, res) => {
+        db.Wishlist.findOne({
+            where:  {
+                id: req.params.id
+            },
+        }).then((dbWishlist) => {
+            res.json(dbWishlist);
+        });
     });
-  });
 
-  app.post("/api/user/wishlist", (req, res) => {
-    db.Wishlist.create(req.body).then(((dbWishlist) => {
-      res.json(dbWishlist);
-    }));
-  });
-
-  app.delete("/api/user/wishlist", (req, res) => {
-    db.Wishlist.destroy(req.body).then((dbWishlist) => {
-      res.json(dbWishlist);
+    app.post("/api/user/wishlist", (req, res) => {
+        db.Wishlist.create(req.body).then(((dbWishlist) => {
+            res.json(dbWishlist);
+        }));
     });
-  });
+
+    app.delete("/api/user/wishlist", (req, res) => {
+        db.Wishlist.destroy(req.body).then((dbWishlist) => {
+            res.json(dbWishlist);
+        });
+    });
 };
