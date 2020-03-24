b77ba8816d21483d7ea907a740871b2d10a60185
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-24 10:57:07 -0400

    format edit

--- config/passport.js
+++ config/passport.js
@@ -17,9 +17,8 @@ passport.use(new LocalStrategy(
         return done(null, false, {
           message: "Incorrect username."
         });
-      }
       // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+      } else if (!dbUser.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
