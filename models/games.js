92009838c7c3599563e0e8e797cac411508d9c52
Marc Maldonado <marc@telecomelite.com>
2020-03-24 20:39:46 -0400

    gamesfile Updated

--- models/games.js
+++ models/games.js
@@ -1,31 +1,31 @@
 module.exports = function(sequelize, DataTypes){
-  // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
-  const Games = sequelize.define("Games",{
+    // pass in id, Name, description, Released, Metacritic, background_image, esrb_rating.name
+    const Games = sequelize.define("Games",{
         gameID: DataTypes.INTEGER,
-    platformsID: DataTypes.INTEGER,
-    storeID: DataTypes.INTEGER,
-    developergameID: DataTypes.INTEGER,
-    genresID: DataTypes.INTEGER,
-    tagID:DataTypes.INTEGER,
-    publishergameID:DataTypes.STRING,
-  },{
-    timestamps: false
-  });
-  Games.associate = function(models){
+        platformsID: DataTypes.INTEGER,
+        storeID: DataTypes.INTEGER,
+        developergameID: DataTypes.INTEGER,
+        genresID: DataTypes.INTEGER,
+        tagID:DataTypes.INTEGER,
+        publishergameID:DataTypes.STRING,
+    },{
+        timestamps: false
+    });
+    Games.associate = function(models){
     console.log(models);
     //Games.belongsToMany(models.Platforms, {through: "Games_Platform"});
-    Games.belongsToMany(models.Users, {through: "User_Wishlist"});
-    Games.belongsToMany(models.Users, {through: "User_Library"});
-    Games.belongsTo(models.Developer,{
-      foreignKey: {
-        allowNull: false
-      }});
-    Games.belongsTo(models.Publisher,{
-      foreignKey: {
-        allowNull: false
-      }});
-  };
-  return Games;
+        Games.belongsToMany(models.Users, {through: "User_Wishlist"});
+        Games.belongsToMany(models.Users, {through: "User_Library"});
+        Games.belongsTo(models.Developer,{
+        foreignKey: {
+            allowNull: false
+        }});
+        Games.belongsTo(models.Publisher,{
+        foreignKey: {
+            allowNull: false
+        }});
+    };
+    return Games;
 };
     	
 
