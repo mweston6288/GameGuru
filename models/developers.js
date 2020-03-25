3eeaf3727751defe72acef000c5db847bc715f2d
Marc Maldonado <marc@telecomelite.com>
2020-03-24 20:32:28 -0400

    Ok I think Ihave the developer file now

--- models/developers.js
+++ models/developers.js
@@ -1,10 +1,10 @@
 module.exports = function(sequelize, DataTypes){
-
-        const Developer = sequelize.define("Developer",{
-            DeveloperID: DataTypes.INTEGER,
+    const Developer = sequelize.define("Developer",{
+        DeveloperID: DataTypes.INTEGER,
     });
-        Developer.associate = function(models){
-        Developer.hasMany(models.Games);
-    };
-        return Developer;
+
+  Developer.associate = function(models){
+    Developer.hasMany(models.Games);
+  };
+  return Developer;
 };
