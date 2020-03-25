6fdb3fb7d66845579a831bd1409ebf86415e91b8
Marc Maldonado <marc@telecomelite.com>
2020-03-24 20:19:41 -0400

    checking deveolpers fiel

--- models/developers.js
+++ models/developers.js
@@ -1,11 +1,10 @@
 module.exports = function(sequelize, DataTypes){
 
-  const Developer = sequelize.define("Developer",{
-    DeveloperID: DataTypes.INTEGER,
-  });
-    Developer.associate = function(models){
+        const Developer = sequelize.define("Developer",{
+            DeveloperID: DataTypes.INTEGER,
+    });
+        Developer.associate = function(models){
         Developer.hasMany(models.Games);
     };
     return Developer;
 };
-    
