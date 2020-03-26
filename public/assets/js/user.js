aa893a924065564d7dd150617d07e8690c2f0485
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-26 12:35:21 -0400

    Marked where api call is made

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -26,6 +26,7 @@ $(document).ready(function () {
     }
 
     function createMainResult(data){
+        console.log(data);
         const body = $("#searchResults");
         const divContainer = $("<div>");
         const divRow = $("<div>");
@@ -33,6 +34,9 @@ $(document).ready(function () {
         const pDeveloper = $("<div>");
         const pGenre = $("<p>");
         const pDescription = $("<p>");
+        const wishlistButton = $("<button>");
+        const libraryButton = $("<button>");
+        const img = $("<img>");
 
         divContainer.attr("class", "container");
         divRow.attr("class", "row");
@@ -40,9 +44,12 @@ $(document).ready(function () {
         pDeveloper.attr("id", "developer");
         pGenre.attr("id", "genre");
         pDescription.attr("id", "description");
+        wishlistButton.attr("id", "wishlist-add");
+        libraryButton.attr("id", "library-add");
+        img.attr("src", data.background_image);
 
         pName.text(data.name);
-        pDescription.text(data.description);
+        pDescription.text(data.description_raw);
         data.developers.forEach(function (developers) {
             const dev = $("<p>");
             dev.text(developers.name);
@@ -53,18 +60,27 @@ $(document).ready(function () {
             genre.text(genres.name);
             pGenre.append(genre);
         });
+        wishlistButton.text("Add to Wishlist");
+        libraryButton.text("Add to library");
+
         body.append(divContainer);
         divContainer.append(divRow);
+        divRow.append(img);
         divRow.append(pName);
         divRow.append(pDeveloper);
         divRow.append(pGenre);
         divRow.append(pDescription);
+        divRow.append(wishlistButton);
+        divRow.append(libraryButton);
+
     }
     function displayResults(mainResult, suggestedResult) {
+        $("#searchResults").empty();
         createMainResult(mainResult);
         createSubResult(suggestedResult);
     }
 
+    // This is where I'm calling the API
     function getName(searchTerm) {
         let queryURL = "https://api.rawg.io/api/games/" + searchTerm.replace(/ /g, "-");
         $.ajax({
