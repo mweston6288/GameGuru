6e7667c5af7a1c208d39d631132f4e19442ee946
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-03-25 22:18:46 -0400

    Managed to jurry rig something that can display your main search query and several suggested results

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -2,103 +2,103 @@ $(document).ready(function () {
     const search = $("form.game-search");
     const searchTerm = $("input#user-input");
     const searchType = $("select#search-type");
-    let searchResults;
-    // let userID;
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
+
     $.get("/api/user_data").then(function (data) {
         $(".username").text(data.username);
         userID = data.id;
     });
-    function displayResults(searchResults){
+    function createSubResult(data){
         const body = $("#searchResults");
-        console.log(body);
-        searchResults.forEach(function(element){
-            console.log(element);
+        data.results.forEach(function (element) {
             const divContainer = $("<div>");
             const divRow = $("<div>");
             const pName = $("<p>");
-            const pDeveloper = $("<div>");
-            const pGenre = $("<p>");
-            const pDescription = $("<p>");
 
             divContainer.attr("class", "container");
             divRow.attr("class", "row");
             pName.attr("id", "name");
-            pDeveloper.attr("id", "developer");
-            pGenre.attr("id", "genre");
-            pDescription.attr("id", "description");
 
             pName.text(element.name);
-            pDescription.text(element.description);
-            element.developers.forEach(function(developers){
-                const dev = $("<p>");
-                dev.text(developers.name);
-                pDeveloper.append(dev);
-            });
-            element.genres.forEach(function (genres) {
-                const genre = $("<p>");
-                genre.text(genres.name);
-                pGenre.append(genre);
-            });
             body.append(divContainer);
             divContainer.append(divRow);
             divRow.append(pName);
-            divRow.append(pDeveloper);
-            divRow.append(pGenre);
-            divRow.append(pDescription);
-
         });
     }
-    function getName(searchTerm){
-        let queryURL = "https://api.rawg.io/api/games/"+searchTerm.replace(/ /g, "-");
+
+    function createMainResult(data){
+        const body = $("#searchResults");
+        const divContainer = $("<div>");
+        const divRow = $("<div>");
+        const pName = $("<p>");
+        const pDeveloper = $("<div>");
+        const pGenre = $("<p>");
+        const pDescription = $("<p>");
+
+        divContainer.attr("class", "container");
+        divRow.attr("class", "row");
+        pName.attr("id", "name");
+        pDeveloper.attr("id", "developer");
+        pGenre.attr("id", "genre");
+        pDescription.attr("id", "description");
+
+        pName.text(data.name);
+        pDescription.text(data.description);
+        data.developers.forEach(function (developers) {
+            const dev = $("<p>");
+            dev.text(developers.name);
+            pDeveloper.append(dev);
+        });
+        data.genres.forEach(function (genres) {
+            const genre = $("<p>");
+            genre.text(genres.name);
+            pGenre.append(genre);
+        });
+        body.append(divContainer);
+        divContainer.append(divRow);
+        divRow.append(pName);
+        divRow.append(pDeveloper);
+        divRow.append(pGenre);
+        divRow.append(pDescription);
+    }
+    function displayResults(mainResult, suggestedResult) {
+        createMainResult(mainResult);
+        createSubResult(suggestedResult);
+    }
+
+    function getName(searchTerm) {
+        let queryURL = "https://api.rawg.io/api/games/" + searchTerm.replace(/ /g, "-");
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).then(function (response) {
-            searchResults.push(response);
-            queryURL = "https://api.rawg.io/api/games/" + response.id + "/suggested";
+        }).then(function (searchResponse) {
+            queryURL = "https://api.rawg.io/api/games/" + searchResponse.id + "/suggested";
             $.ajax({
                 url: queryURL,
                 method: "GET"
-            }).then(function (response) {
-                response.results.forEach(function(element){
-                    searchResults.push(element);
-                });
-                console.log(searchResults);
-                displayResults(searchResults);
+            }).then(function (simResponse) {
+                displayResults(searchResponse, simResponse);
             });
         });
     }
-    function getDeveloper(searchTerm){
-        const queryURL = "https://api.rawg.io/api/games/" + searchTerm.replace(/ /g, "-");
-        console.log(queryURL);
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-        });
-    }
-    function searchAPI(searchTerm, searchType){
-        switch (searchType){
+    function searchAPI(searchTerm, searchType) {
+        switch (searchType) {
         case "name":
             getName(searchTerm);
             break;
         case "developer":
-            getDeveloper(searchTerm);
+            //getDeveloper(searchTerm);
             break;
         default: return;
         }
     }
-    search.on("submit", function(event){
+    search.on("submit", function (event) {
         event.preventDefault();
         searchResults = [];
-        const searchData= {
+        const searchData = {
             searchTerm: searchTerm.val().trim(),
             searchType: searchType.val().trim()
         };
-        if (!searchData.searchTerm){
+        if (!searchData.searchTerm) {
             return;
         }
         searchAPI(searchData.searchTerm, searchData.searchType);
