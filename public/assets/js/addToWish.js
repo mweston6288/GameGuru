170c5088ebe98e337d54a541df58a755f0f31900
Marc Maldonado <marc@telecomelite.com>
2020-03-26 23:54:42 -0400

    need to figure out why my lines are not there for travis

--- public/assets/js/addToWish.js
+++ public/assets/js/addToWish.js
@@ -5,21 +5,20 @@ $("#wishlist-add").on("click", (event) => {
     const Url = "https://api.rawg.io/api/games/"+gName.replace(/ /g, "-");
     $.get(Url)
         .then((req) => {
-                event.preventDefault();
-                const newGame = {
-                    gameID: req.id,
-                    platformID: req.platforms.id,
-                    storeID: req.stores.id,
-                    developergameID: req.developers.id,
-                    genresID: req.genres.id,
-                    tagID: req.tags.id,
-                    publishergameID: req.publishers.id,
-        
-                };
-                console.log(newGame);
-                $.post("/api/user/game", newGame)
-                .then((data) => {
-                    console.log(data)
-          });
+            event.preventDefault();
+            const newGame = {
+                gameID: req.id,
+                platformID: req.platforms.id,
+                storeID: req.stores.id,
+                developergameID: req.developers.id,
+                genresID: req.genres.id,
+                tagID: req.tags.id,
+                publishergameID: req.publishers.id,
+            };
+            console.log(newGame);
+            $.post("/api/user/game", newGame)
+            .then((data) => {
+                    console.log(data);
+                });
     }); 
-})
+});
