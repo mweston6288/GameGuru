bcd07ef8374171a8a8c06e42084e9c55d26766ee
Marc Maldonado <marc@telecomelite.com>
2020-03-27 11:35:42 -0400

    Got it to pull ids into newGame object but get this error Unhandled rejection SequelizeValidationError: notNull Violation: Games.PublisherId cannot be null

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -45,6 +45,57 @@ $(document).ready( () => {
     };
 
     const addToWishlist = (gameID)=>{
+        const Url = "https://api.rawg.io/api/games/"+ gameID;
+        $.get(Url)
+        .then((req) => {
+            event.preventDefault();
+            console.log(req.genres)
+            const pArray = req.platforms;
+            const pArrayId = [];
+            pArray.forEach((element) => {
+                pArrayId.push((element.platform.id))
+            });
+            const sArray = req.stores;
+            const sArrayId = [];
+            sArray.forEach((element) => {
+                sArrayId.push(element.id)
+            });
+            const dArray = req.developers;
+            const dArrayId = [];
+            dArray.forEach((element) => {
+                dArrayId.push(element.id)
+            })
+            const gArray = req.genres;
+            const gArrayId = [];
+            gArray.forEach((element) => {
+                gArrayId.push(element.id)
+            });
+            const tArray = req.tags;
+            const tArrayId = [];
+            tArray.forEach((element) => {
+                tArrayId.push(element.id)
+            })
+            const pubArray = req.publishers;
+            const pubArrayId = [];
+            pubArray.forEach((element) => {
+                pubArrayId.push(element.id)
+            })
+            const newGame = {
+                gameID: req.id,
+                platformID: pArrayId,
+                storeID: sArrayId,
+                developergameID: dArrayId,
+                genresID: gArrayId,
+                tagID: tArrayId,
+                publishergameID: pubArrayId,
+                userID: userID,
+            };
+            console.log(newGame);
+            $.post("/api/user/game", newGame)
+                .then((data) => {
+                    console.log(data);
+                });
+        });
         console.log(gameID);
     };
     const makeNewSearchEvent =() => {
--- routes/games-api-routes.js
+++ routes/games-api-routes.js
@@ -2,41 +2,39 @@ const db = require("../models");
 
 module.exports = (app) => {
     app.get("https://api.rawg.io/api/games", (req, res) => {
-        let devQuery = {};
-        if (req.devQuery.developer_id) {
-            devQuery.DeveloperId = req.devQuery.developer_id;
-        }
-        let pubQuery = {};
-        if (req.pubQuery.publisher_id) {
-            pubQuery.PublisherId = req.publisher_id.publisher_id;
-        }
+        
+        
         db.Games.findAll({
-            where: devQuery, pubQuery,
-            include: [db.Developer] [db.Publisher]
+           
         }).then((dbGames) => {
             res.json(dbGames);
         });
     });
 
     app.get("https://api.rawg.io/api/games/:name", (req, res) => {
-        let devQuery = {};
-        if (req.devQuery.developer_id) {
-            devQuery.DeveloperId = req.devQuery.developer_id;
-        }
-        let pubQuery = {};
-        if (req.pubQuery.publisher_id) {
-            pubQuery.PublisherId = req.publisher_id.publisher_id;
-        }
         db.Games.findOne({
             where:  {
                 name: req.params.name
             },
-            devQuery, pubQuery,
-            include: [db.Developer] [db.Publisher]
+            
         }).then((dbGames) => {
             res.json(dbGames);
         });
     });
+    app.post("/api/user/game", (req, res) => {
+        //console.log("Game Data:");
+        console.log(req.body);
+        db.Games.create({
+            gameID: req.body.gameID,
+            plaformID: req.body.plaformID,
+            storeID: req.body.storeID,
+            developergameID: req.body.developergameID,
+            genresID: req.body.genresID,
+            tagID: req.body.tagID,
+            publishergameID: req.body.publishergameID,
+            userID: req.body.userID,
+        })
+    })
 
 
 };
